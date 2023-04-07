2c67032a5e5fed40744bf7d4aec068d855b3c1b4
Calvin Gomes <calvingomes045@gmail.com>
2023-04-07 01:29:44 +0100

    Built custom component

--- React-Basics/my-app/src/App.js
+++ React-Basics/my-app/src/App.js
@@ -1,6 +1,9 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
 function App() {
   return (
     <div>
+      <ExpenseItem />
       <h2>Let's get started!</h2>
     </div>
   );
---
+++ React-Basics/my-app/src/Components/ExpenseItem.js
@@ -0,0 +1,7 @@
+const ExpenseItem = () => {
+    return (
+        <h2>Expense of an Item</h2>
+    )
+}
+
+export default ExpenseItem;
