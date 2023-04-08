d09b601b37b6c5aba754daf2a09fb34c0d3665fc
Calvin Gomes <calvingomes045@gmail.com>
2023-04-08 02:31:14 +0100

    splitting components

--- React-Basics/my-app/src/Components/ExpenseItem/ExpenseItem.jsx
+++ React-Basics/my-app/src/Components/ExpenseItem/ExpenseItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({name, date, price}) => {
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{name}</h2>
         <div className="expense-item__price">{price}</div>
