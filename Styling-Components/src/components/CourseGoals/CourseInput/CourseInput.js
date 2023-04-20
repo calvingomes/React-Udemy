c1c37694be61a454e55a57cd349ff4d0f7ebba73
Calvin Gomes <calvingomes045@gmail.com>
2023-04-20 17:04:54 +0100

    styled comp approach 2

--- Styling-Components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Styling-Components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,6 +10,7 @@ const CourseInput = (props) => {
 
     & label {
       font-weight: bold;
+      color: ${props => (props.isValid ? 'red' : 'black')};
       display: block;
       margin-bottom: 0.5rem;
     }
@@ -17,8 +18,8 @@ const CourseInput = (props) => {
     & input {
       display: block;
       width: 100%;
-      border: 1px solid #ccc;
-      background: white;
+      border: 1px solid ${props => (props.isValid ? 'red' : '#ccc')};
+      background: ${props => (props.isValid ? '#ffd7d7' : 'white')};
       font: inherit;
       line-height: 1.5rem;
       padding: 0 0.25rem;
@@ -28,15 +29,6 @@ const CourseInput = (props) => {
       outline: none;
       background: #fad0ec;
       border-color: #8b005d;
-    }
-
-    &.invalid input {
-      border-color: red;
-      background: #ffd7d7;
-    }
-
-    &.invalid label {
-      color: red;
     }
   `;
 
@@ -63,7 +55,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={`${!isValid ? "invalid" : ""}`}>
+      <FormControl isValid = {!isValid} >
         <label>Course Goal</label>
         <input
           type="text"
